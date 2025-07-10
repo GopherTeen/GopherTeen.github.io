7cc026a49ff56661dabf0a39263c357c27aea746
Gopher <1900271083@qq.com>
2025-07-10 20:02:46 +0800

    Site updated: 2025-07-10 20:02:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/10/hello-world/","2025/07/10/复变函数与积分变换/","2025/07/10/数据结构/","2025/07/10/概率论与数理统计/","2025/07/10/计算机网络/"];function toRandomPost(){
+var posts=["2025/07/10/数据结构/","2025/07/10/复变函数与积分变换/","2025/07/10/组合数学/","2025/07/10/计算机网络/","2025/07/10/概率论与数理统计/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
